193f0dd1c7db62a8a7475845b78c3f7d7a20f305
x0- <hxsushi@126.com>
2023-09-26 13:54:27 +0100

    re-implement triple pivots qsort

--- src/qsort.rs
+++ src/qsort.rs
@@ -1,7 +1,7 @@
 // #![allow(dead_code)]
 #![allow(unused)]
 
-const DEBUG_INSERTION_SORT_THRESHOLD: usize = 3;
+const DEBUG_INSERTION_SORT_THRESHOLD: usize = 5;
 const RELEASE_INSERTION_SORT_THRESHOLD: usize = 27;
 const QUICKSORT_STACK_SIZE: usize = 64;
 
@@ -129,76 +129,120 @@ pub fn triple_pivot_quicksort<T: PartialOrd + Copy>(arr: &mut [T]) {
 
 	unsafe {
 
-		let pivot1: *mut T = &mut arr[left];
-		// let pivot2: *mut T = &mut arr[(left + right) / 2];
+		let mut mid = (left + right) / 2;
+		let pivot1: *mut T = &mut arr[mid - 1];
+		let pivot2: *mut T = &mut arr[mid];
 		let pivot3: *mut T = &mut arr[right];
 
-		// if *pivot1 > *pivot2 { arr.swap(left, (left + right) / 2); }
-		if *pivot1 > *pivot3 { arr.swap(left, right); }
-		// if *pivot2 > *pivot3 { arr.swap((left + right) / 2, right); }
+		if *pivot1 > *pivot2 { arr.swap(mid - 1, mid); }
+		if *pivot1 > *pivot3 { arr.swap(mid - 1, right); }
+		if *pivot2 > *pivot3 { arr.swap(mid, right); }
 
 		let pivot1_val = *pivot1.clone();
-		// let pivot2_val = *pivot2.clone();
+		let pivot2_val = *pivot2.clone();
 		let pivot3_val = *pivot3.clone();
+		
 		// partition indexes
-		let mut less = left + 1;
-		let mut greater = right - 1;
 
-		// sorting
-		let mut k = less;
-		while k <= greater {
-			if arr[k] <= pivot1_val {
-					arr.swap(k, less);
-					less = less + 1;
+		// |            left part          | mid |          right part          |
+		// | partition 0 | ... | partition 1 | partition 2 | ... | partition 3 |
+
+		// TODO: optimize the following code (turn them into loops or function calls)
+		// ==========================================================================
+		// put left part elements less than pivot1 into partition 0
+		let mut pivot1_index = left;
+		for i in left..mid - 1 {
+			if arr[i] <= pivot1_val {
+				arr.swap(i, pivot1_index);
+				pivot1_index += 1;
 			}
-			else {
-				if arr[k] >= pivot3_val {
-					while k < greater && arr[greater] > pivot3_val {
-						greater = greater - 1;
-					}
-					// swap it with arr[k]
-					arr.swap(k, greater);
-					greater = greater - 1;
+		}
+		arr.swap(pivot1_index, mid - 1);
 
-					// if the swapped element is less than pivot1
-					// then swap it with arr[less]
-					if arr[k] <= pivot1_val {
-						arr.swap(k, less);
-						less = less + 1;
-					}
+		// put right part elements greater than pivot3 on the rightmost side
+		arr.swap(mid + 1, right);
+		let mut pivot3_index = right;
+		for i in (mid + 2..=right).rev() {
+			if arr[i] >= pivot3_val {
+				arr.swap(i, pivot3_index);
+				pivot3_index -= 1;
+			}
+		}
+		arr.swap(pivot3_index, mid + 1);
+		// ==========================================================================
+		
+		let mut pivot2_index = mid;
+		if pivot1_index + 2 < pivot3_index {
+			arr.swap(pivot3_index - 1, mid);
+
+			let mut l = pivot1_index + 1;
+			for k in pivot1_index + 1..pivot3_index - 1 {
+				if arr[k] <= pivot2_val {
+					arr.swap(l, k);
+					l += 1;
 				}
 			}
-			k = k + 1;
+			arr.swap(l, pivot3_index - 1);
+			pivot2_index = l;
 		}
+
+		// new mid as pivot2_index
+		mid = pivot2_index;
+
+		// partition arr[left..mid] and arr[mid + 1..right]
+		// if mid == 0, then arr[left..mid] is empty
+		// if mid == right - 1, then arr[mid + 1..right] is empty
+
+		// partition arr[left..pivot2_index]
+		// move pivot1 to the end of | partition 0 | ... | partition 1 |
 		
-		arr.swap(less - 1, left);
-		arr.swap(greater + 1, right);
+		// DO NOT remove equal sign, otherwise it will cause error
+		if pivot2_index >= left {
+			arr.swap(mid - 1, pivot1_index);
 
-		if less > left + 2 {
-			triple_pivot_quicksort(&mut arr[left..=less - 2]);
-		}
-
-		if greater + 2 < right {
-			triple_pivot_quicksort(&mut arr[greater + 2..=right]);
-		}
-
-		if less < greater && pivot1_val < pivot3_val {
-			let mut i = less;
-			let pivot2_val = arr[greater];
-			for j in less..greater {
-				if arr[j] <= pivot2_val {
+			let mut i = left;
+			for j in left..mid - 1 {
+				if arr[j] <= pivot1_val {
 					arr.swap(i, j);
 					i += 1;
 				}
 			}
-			arr.swap(i, greater);
-			// i > less + 1 and i + 1 < greater to enter insertion sort branch
-			if i > less + 1 {
-				triple_pivot_quicksort(&mut arr[less..=i - 1]);
+			arr.swap(i, mid - 1);
+			pivot1_index = i;
+		}
+		// partition arr[pivot2_index + 1..right]
+
+		// move pivot3 to the end of | partition 2 | ... | partition 3 |
+		// DO NOT remove equal sign, otherwise it will cause error
+		if pivot3_index <= right {
+			arr.swap(right, pivot3_index);
+
+			let mut i = mid + 1;
+			for j in mid + 1..right {
+				if arr[j] <= pivot3_val {	
+					arr.swap(i, j);
+					i += 1;
+				}
 			}
-			if i + 1 < greater {
-				triple_pivot_quicksort(&mut arr[i + 1..=greater]);
-			}
+			arr.swap(i, right);
+			pivot3_index = i;
+		}
+
+		// all the followings have to be xx + C < yy, C = 1, C > 1 will cause error 
+		if pivot1_index > left + 1 {
+			triple_pivot_quicksort(&mut arr[left..pivot1_index]);
+		}
+
+		if pivot1_index + 1 < pivot2_index {
+			triple_pivot_quicksort(&mut arr[pivot1_index + 1..pivot2_index]);
+		}
+
+		if pivot2_index + 1 < pivot3_index {
+			triple_pivot_quicksort(&mut arr[pivot2_index + 1..pivot3_index]);
+		}
+
+		if pivot3_index + 1 < right {
+			triple_pivot_quicksort(&mut arr[pivot3_index + 1..=right]);
 		}
 
 	}
--- src/test.rs
+++ src/test.rs
@@ -37,13 +37,33 @@ mod test {
 	fn test_triple_pivot_quicksort() {
 		let mut arr = [0.5835883f32, 0.71631217, 0.8832437, 0.3880486, 0.021315217, 0.53300536, 0.20367897, 0.27174407, 0.96776694, 0.25351256];
 		let dur = time_it(|| triple_pivot_quicksort(&mut arr));
+        #[cfg(debug_assertions)]
+        println!("arr: {:?}", arr);
 		assert_eq!(is_sorted(&arr), true);
 		println!("triple pivot quick sort cost: {:?}ns", dur);
 
-        let mut arr: Vec<f32> = default_vec(1_000);
+        let mut arr = [0.96378565f32, 0.3529173, 0.547055, 0.9667923, 0.31933308, 0.8284124, 0.14586675, 0.48742378, 0.5244448, 0.28129673];
         let dur = time_it(|| triple_pivot_quicksort(&mut arr));
+        #[cfg(debug_assertions)]
+        println!("arr: {:?}", arr);
         assert_eq!(is_sorted(&arr), true);
-        println!("triple pivot quick sort on 1k floats cost: {:?}ns", dur);
+        println!("triple pivot quick sort cost: {:?}ns", dur);
+
+        let mut arr: Vec<f32> = default_vec(10);
+        #[cfg(debug_assertions)]
+        println!("arr before: {:?}", arr);
+        let dur = time_it(|| triple_pivot_quicksort(&mut arr));
+        #[cfg(debug_assertions)]
+        println!("arr after: {:?}", arr);
+        assert_eq!(is_sorted(&arr), true);
+        println!("triple pivot quick sort on 10 floats cost: {:?}ns", dur);
+
+        let mut arr: Vec<f32> = default_vec(1_00);
+        let dur = time_it(|| triple_pivot_quicksort(&mut arr));
+        #[cfg(debug_assertions)]
+        println!("arr: {:?}", arr);
+        assert_eq!(is_sorted(&arr), true);
+        println!("triple pivot quick sort on 100 floats cost: {:?}ns", dur);
 
         let mut arr: Vec<f32> = default_vec(1_000_000);
         let dur = time_it(|| triple_pivot_quicksort(&mut arr));
