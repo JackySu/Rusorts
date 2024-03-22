2f2937d99582a6c33ae2d65439505884916ab6a6
iulx0 <hxsushi@gmail.com>
2024-03-22 23:29:51 +0000

    formulas, formulas and formulas

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use std::env;
 
 fn main() {
     if cfg!(target_arch = "x86_64") {
+        println!("cargo:warning=Enabling AVX2 and FMA for x86_64 target");
         env::set_var("RUSTFLAGS", "-C target-feature=+avx2,+fma");
     }
 }
